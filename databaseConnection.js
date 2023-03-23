d3d7d78c7859b1262a73b2c3cfd3279818c5cc86
Royce Coviello <coviello.royce@gmail.com>
2023-03-23 14:57:24 -0700

    heroku to qoddi

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,31 +1,45 @@
-const mysql = require('mysql2');
-
-const is_heroku = process.env.IS_HEROKU || false;
-
-const dbConfigHeroku = {
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
-	multipleStatements: false,
-	namedPlaceholders: true
-};
-
-const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "lab_example",
-	multipleStatements: false,
-	namedPlaceholders: true
-};
-
-if (is_heroku) {
-	var database = mysql.createPool(dbConfigHeroku);
+const MongoClient = require("mongodb").MongoClient;
+const is_qoddi = process.env.IS_QODDI || false;
+const qoddiURI =
+"mongodb+srv://admin:TJegPTvGhuNMMnPL@cluster0.ztfkq3k.mongodb.net/?retryWrites=true&w=majority"
+const localURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority"
+if (is_qoddi) {
+var database = new MongoClient(qoddiURI,
+{useNewUrlParser: true, useUnifiedTopology: true});
 }
 else {
-	var database = mysql.createPool(dbConfigLocal);
+var database = new MongoClient(localURI,
+{useNewUrlParser: true, useUnifiedTopology: true});
 }
+
+// const mysql = require('mysql2');
+
+// const is_qoddi = process.env.IS_HEROKU || false;
+
+// const dbConfigHeroku = {
+// 	host: "us-cdbr-east-03.cleardb.com",
+// 	user: "b1ab7fb2ee03bc",
+// 	password: "2a484a2d",
+// 	database: "heroku_3d208ad4bd6f421",
+// 	multipleStatements: false,
+// 	namedPlaceholders: true
+// };
+
+// const dbConfigLocal = {
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "Password",
+// 	database: "lab_example",
+// 	multipleStatements: false,
+// 	namedPlaceholders: true
+// };
+
+// if (is_qoddi) {
+// 	var database = mysql.createPool(dbConfigHeroku);
+// }
+// else {
+// 	var database = mysql.createPool(dbConfigLocal);
+// }
 
 module.exports = database;
 		
--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,10 +1,10 @@
-const is_heroku = process.env.IS_HEROKU || false;
+const is_qoddi = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = "mongodb+srv://admin:TJegPTvGhuNMMnPL@cluster0.ztfkq3k.mongodb.net/?retryWrites=true&w=majority";
 
 const dbConfigLocal = "mysql://root:Password@localhost/lab_example";
 
-if (is_heroku) {
+if (is_qoddi) {
 	var databaseConnectionString = dbConfigHeroku;
 }
 else {
