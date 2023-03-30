ed5245a4613ae9404fd01dc2d296e47d507a66da
Royce Coviello <coviello.royce@gmail.com>
2023-03-29 22:42:15 -0700

    working

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,7 +2,7 @@ const MongoClient = require("mongodb").MongoClient;
 const is_qoddi = process.env.IS_QODDI || false;
 const qoddiURI =
 "mongodb+srv://admin:TJegPTvGhuNMMnPL@cluster0.ztfkq3k.mongodb.net/?retryWrites=true&w=majority"
-const localURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority"
+const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority"
 if (is_qoddi) {
 var database = new MongoClient(qoddiURI,
 {useNewUrlParser: true, useUnifiedTopology: true});
